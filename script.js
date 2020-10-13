947be16839b23856eeeeff36ca2d542c9730393f
BRaines91 <70935080+BRaines91@users.noreply.github.com>
2020-10-13 00:14:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var questions = [{
-    question: "1. How do you write 'Hello World' in an alert box?",
-    choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-    correctAnswer: 3
+    question: "1. Inside which HTML element do we put the Javascript",
+    choices: ["javascript", "js", "scripting", "script"],
+    correctAnswer: 4
 }, {
     question: "2. How to empty an array in JavaScript?",
     choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
-    correctAnswer: 2
+    correctAnswer: 3
 }, {
-    question: "3. What function to add an element at the begining of an array and one at the end?",
-    choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
+    question: "3. Where is the correct place to insert javascript",
+    choices: ["Both the head section and the body section are correct", "The body section", "the head section", "The div section"],
     correctAnswer: 1
 }, {
-    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
-    choices: ["undefined", "0", "prints nothing", "Syntax error"],
-    correctAnswer: 0
+    question: "4. What is the correct syntax for referring to an external script called xxx.js",
+    choices: ["script href=xxx.js", "script src=xxx.js", "script name=xxx.js", " script type=xxx.s"],
+    correctAnswer: 2
 }, {
     question: "5. What would following code return? console.log(typeof typeof 1);",
     choices: ["string", "number", "Syntax error", "undefined"],
